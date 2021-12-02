1e8de5b6fee4299a3fa018ca98a4a77df885a6b6
Nicolas Quintero <nikolas.q@gmail.com>
2021-12-01 23:23:24 -0500

    Organizando los componentes para la vista del proyecto en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/include/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
