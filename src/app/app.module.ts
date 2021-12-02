95c64d0be94295fa5e469dbc5aa9f45874bf593c
Nicolas Quintero <nikolas.q@gmail.com>
2021-12-01 23:47:05 -0500

    Agregando el complemento de bootstrap al proyecto y adicionandolo al angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/include/footer/footer.component';
+import { HeaderComponent } from './components/include/header/header.component';
+import { MenuComponent } from './components/include/menu/menu.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
-      FooterComponent
+      FooterComponent,
+      HeaderComponent,
+      MenuComponent
    ],
   imports: [
     BrowserModule,
