dffd3b164f8972d2cc99f0a3cc31b7a6f8abe113
Nicolas Quintero <nikolas.q@gmail.com>
2021-12-03 19:10:14 -0500

    Registro el sistema para las rutas y las importo a los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from 'src/app/app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,22 +9,26 @@ import { HeaderComponent } from './components/include/header/header.component';
 import { MenuComponent } from './components/include/menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
       FooterComponent,
       HeaderComponent,
-      MenuComponent
+      MenuComponent,
+      HomeComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
-
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
