29b0b28b029c96bf62ed1ad78c050ed4667b73ff
Nicolas Quintero <nikolas.q@gmail.com>
2021-12-03 23:48:49 -0500

    Creando el servicio para conectarme al API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from 'src/app/app.routing';
+import { HttpClientModule} from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/include/footer/footer.component';
 import { HeaderComponent } from './components/include/header/header.component';
 import { MenuComponent } from './components/include/menu/menu.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [	
